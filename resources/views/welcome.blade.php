1bebb8459660c6f4d6c9c0a605865a100616a223
Jovert Palonpon <palonponjovertlota@gmail.com>
2020-03-08 14:11:08 +0800

    chore: Trigger upload-assets workflow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,2 @@
 <x-layout>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem tempora ut eligendi maxime explicabo maiores nobis commodi pariatur provident iste, optio voluptates nesciunt veritatis molestiae quis eaque vero minima. Perspiciatis!
 </x-layout>
